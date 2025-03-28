25fcf4390babc9455a3587698dab10b9edae7901
FirstTirr <fathiradzans@gmail.com>
2025-03-28 18:18:12 +0700

    massage

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=0.89" />
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=0.9"
+        />
         <meta name="keywords" content="SMKN 4 Payakumbuh" />
       </head>
       <body

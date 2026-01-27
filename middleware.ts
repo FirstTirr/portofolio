14e9bd59c1bfa8078ece2eda4bac6883474be84d
FirstTirr <fathiradzans@gmail.com>
2026-01-27 15:17:24 +0700

    re-enable next-auth login protection

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
-// import NextAuth from "next-auth";
-// import { authConfig } from "./auth.config";
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
 
-// export default NextAuth(authConfig).auth;
-
-export function middleware() {}
+export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  // matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-  matcher: [], // Disabled middleware for now
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
